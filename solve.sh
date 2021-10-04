cfc8b633bb49701a6d6b0ce48fb76172e96eb84b
Clement Hamada <clementhamada@protonmail.com>
2021-10-04 18:59:32 +0200

    Cleanup solve.sh output

--- solve.sh
+++ solve.sh
@@ -7,18 +7,24 @@ vm_ssh_port=4242
 
 flag=""
 
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
 parse_flag()
 {
-	tee /dev/stderr | cut -b 33- -
+	cut -b 33- -
 }
 
 for level in level*
 do
 	prev_flag="$flag"
-	pushd "$level"
-		echo "Solving $level..."
-		flag="$(./solve.sh "$vm_ip" "$vm_ssh_port" "$prev_flag" | parse_flag)"
-		echo "$level solved: flag: '$flag'!"
-	popd
+	flag="$("$level/solve.sh" "$vm_ip" "$vm_ssh_port" "$prev_flag" | parse_flag)"
+	i=$((${level:5} + 1))
+	echo "level$i:$flag"
 done
 
