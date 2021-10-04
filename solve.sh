4e7a4136aae34214c95abe3ab1d7a0c86281e01b
Clément Hamada <clementhamada@protonmail.com>
2021-10-04 22:07:51 +0200

    solve: Return error if flag is empty

--- solve.sh
+++ solve.sh
@@ -26,8 +26,8 @@ do
 	then
 		prev_flag="$flag"
 		flag="$("$level/solve.sh" "$vm_ip" "$vm_ssh_port" "$prev_flag" | parse_flag)"
+		[ -z $flag ] && echo "Could not solve $level: empty flag!" >&2 && exit 1
 		n=$((${level:5} + 1))
 		printf 'level%02u:%s\n' "$n" "$flag"
 	fi
 done
-
