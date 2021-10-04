600b5b1e2eb470e2dbfe2bb415ff394c112230d2
Clément Hamada <clementhamada@protonmail.com>
2021-10-04 22:15:10 +0200

    solve: Cache flags instead of resolving

--- solve.sh
+++ solve.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-set -ueo pipefail
+set -euo pipefail
 
 vm_ip=localhost
 vm_ssh_port=4242
@@ -20,14 +20,24 @@ parse_flag()
 	cut -b 33- -
 }
 
-for level in level*
+for level in level*/
 do
-	if [ -x "$level/solve.sh" ]
+	n=$((${level:5} + 1))
+
+	prev_flag="$flag"
+	if [ -f "$level/flag" ]
 	then
-		prev_flag="$flag"
+		read -r flag < "$level/flag"
+
+		printf 'level%02u:%s\n' "$n" "$flag"
+	elif [ -x "$level/solve.sh" ]
+	then
 		flag="$("$level/solve.sh" "$vm_ip" "$vm_ssh_port" "$prev_flag" | parse_flag)"
+
 		[ -z $flag ] && echo "Could not solve $level: empty flag!" >&2 && exit 1
-		n=$((${level:5} + 1))
+
+		echo "$flag" > "$level/flag"
+
 		printf 'level%02u:%s\n' "$n" "$flag"
 	fi
 done
