49165a6cf0e647546c917e2274172aff7394cc7a
Clément Hamada <clementhamada@protonmail.com>
2021-10-11 00:30:38 +0200

    level09: Use port argument instead of hardcode

--- level09/solve.sh
+++ level09/solve.sh
@@ -11,7 +11,7 @@ host="$1"
 port="$2"
 pass="$3"
 
-pass.exp "$pass" scp -P 4242 "$PARENT_DIR/idx_shift.c" level09@localhost:/tmp/idx_shift.c > /dev/null
+pass.exp "$pass" scp -P "$port" "$PARENT_DIR/idx_shift.c" level09@localhost:/tmp/idx_shift.c > /dev/null
 pass=$(sshexec.sh "$host" "$port" level09 "$pass" 'set -e; cd /tmp; gcc -Wall -Wextra -Werror --std=c99 idx_shift.c -o idx_shift; ./idx_shift < ~/token')
 
 sshexec.sh "$host" "$port" flag09 "$pass" 'getflag'
